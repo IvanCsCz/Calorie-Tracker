b4b12e9fb9b0711038e2798ad56ffb0b5d1a5260
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-10-07 21:37:49 -0600

    add deleted action

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,12 @@
-import { ChangeEvent, Dispatch, FormEvent, useState } from "react"
+import { ChangeEvent, Dispatch, FormEvent, useEffect, useState } from "react"
 import { v4 as uuidv4 } from 'uuid'
 import { categories } from "../data/categories"
-import { ActivityActions } from "../reducers/activityReducer"
+import { ActivityActions, ActivityState } from "../reducers/activityReducer"
 import type { Activity } from "../types"
 
 type FormProps = {
-  dispatch: Dispatch<ActivityActions>
+  dispatch: Dispatch<ActivityActions>,
+  state: ActivityState
 }
 
 const initialState: Activity = {
@@ -15,8 +16,15 @@ const initialState: Activity = {
   calories: 0
 }
 
-function Form({dispatch}: FormProps){
+function Form({dispatch, state}: FormProps){
   const [activity, setActivity] = useState<Activity>(initialState)
+
+  useEffect(() => {
+    if(state.activeId){
+      const selectedActivity = state.activities.find(stateActivity => stateActivity.id === state.activeId)
+      setActivity(selectedActivity!)
+    }
+  }, [state.activeId, state.activities])
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
     const isNumberField = ['category', 'calories'].includes(e.target.id)
